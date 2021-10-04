780546c7ac4e73048e294612c8d72cd27a98efca
carfolacam98 <laura.carfolacam@gmail.com>
2021-10-04 17:05:27 -0500

    in master

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get,Param,Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,28 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+  // Esto es lo que cambia en la url
+  @Get('nuevo')
+  newEndpoint(){
+    return 'Yo soy un nuevo endpoint'
+  }
+  @Get('product/:id')
+  getProduct(@Param("id") id:number){
+    return `product ${id}`
+  }
+
+  //En elednpoint query se ve como asi http://localhost:3000/products?limit=100&offset=50
+  //es decir que uno lo asignaaa!!
+  @Get('products')
+  getProducts(@Query('limit') limit:number,
+  @Query('offset') offset:number,
+  @Query('brand') brand:number){
+
+    return `product ${limit} &  ${offset} &  ${brand}`
+  }
+  @Get('categories/:categoryWord/product/:id')
+  getCategories(@Param("id") id:number,@Param("categoryWord") categoryWord:string){
+    return `product ${id} in category ${categoryWord}`
   }
 }
